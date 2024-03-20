b9759ed4020e10fe9854fde186ed10aaecd46984
Cameron Shearer <cameronleeshearer@gmail.com>
2024-03-20 15:25:24 +0000

    urls and front-end start

--- quiz/website/urls.py
+++ quiz/website/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('room/', views.room, name='room'),
+    path('lobby/', views.lobby, name='lobby'),
+    path('settings/', views.settings, name='settings'),
+    path('profile/', views.profile, name='profile')
 ]
--- quiz/website/views.py
+++ quiz/website/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 
+# Will need to pass through params
+
 def home(request):
     return render(request, 'home.html', {})
+
+def room(request):
+    return render(request, 'room.html', {})
+
+def lobby(request):
+    return lobby(request, 'lobby.html', {})
+
+def settings(request):
+    return settings(request, 'settings.html', {})
+
+def profile(request):
+    return profile(request, 'profile.html', {})
